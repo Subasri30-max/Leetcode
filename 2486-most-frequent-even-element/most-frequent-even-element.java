ee585f77b0e6a82aeb22bd316009f5a0a547e1af
Subasri30-max <subiguhan@gmail.com>
2025-04-28 21:10:52 +0530

    Time: 44 ms (10.90%) | Memory: 44.9 MB (77.32%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,13 +1,13 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
         int count=-1;
-        int[] freq=new int[1000001];
+        int[] freq=new int[100001];
         for(int i:nums){
             if(i%2==0)
             freq[i]++;
         }
         int max=0;
-        for(int i=0;i<1000001;i++){
+        for(int i=0;i<100001;i++){
             if(i%2==0)
             if(freq[i]>max){
                 max=freq[i];
